1a24626006b31b0f8c33655f9d5d46288eff194f
Alter0 <dev.alter0@gmail.com>
2019-12-02 14:35:50 +0900

    Upload
    Reference
    https://cafe.naver.com/unityhub/99540

---
+++ AnimationCurveAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Animation Curve",order = 400)]
+public class AnimationCurveAsset : ScriptableObject
+{
+    public AnimationCurve Curve = AnimationCurve.Linear(0, 0, 1, 1);
+
+    public static implicit operator AnimationCurve(AnimationCurveAsset curveAsset )
+    {
+        return curveAsset.Curve;
+    }
+
+    public static implicit operator AnimationCurveAsset(AnimationCurve curveAsset)
+    {
+        AnimationCurveAsset asset = CreateInstance<AnimationCurveAsset>();
+        asset.Curve = curveAsset;
+        return asset;
+    }
+}
